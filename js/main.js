fcfba4bf16e50327c3037791ecb491885e45c526
ogheneruno <orunosaduwa@gmail.com>
2019-07-22 22:45:25 +0100

    home page set up

---
+++ js/main.js
@@ -0,0 +1,27 @@
+(function(){
+    'use strict';
+  
+    class Menu {
+      constructor(settings) {
+        this.menuNode = settings.menuNode;
+      }
+      
+      toggleMenuState(className) {      
+        if (typeof className !== 'string' || className.length === 0) {
+          return console.log('you did not give the class for toggleState function');
+        } 
+        return  this.menuNode.classList.toggle(className);
+      }
+    }
+  
+    const jsMenuNode = document.querySelector('.menu');
+    const demoMenu = new Menu ({
+      menuNode: jsMenuNode
+    });
+    
+    function callMenuToggle(event) {
+      demoMenu.toggleMenuState('menu_activated');
+    }
+    
+    jsMenuNode.querySelector('.menu__hamburger').addEventListener('click', callMenuToggle);
+  })();
